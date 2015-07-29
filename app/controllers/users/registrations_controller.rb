335ac15dc610f1a121c52558bfc35704d3aee6c1
madcloud01 <cgurban@gmail.com>
2015-07-29 03:17:40 +0000

    Improved users index page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :select_plan, only: :new
   
      def create
-     super do |resource|
+         super do |resource|
       if params[:plan]
          resource.plan_id = params[:plan]
          if resource.plan_id == 2
